714517c9f115914fd2124317cb8515ba31c8eef1
Dorian Bell II <BellJrDev@gmail.com>
2025-08-13 17:21:38 -0400

    Cleared branch

--- docs/index.md
+++
@@ -1,7 +0,0 @@
-# Welcome to C-Script Docs
-
-Typescript Ergonomics. C++ Power.
-
-This documentation covers all you need to know about C-Script, including API references, tutorials, and examples.
-
-Start by exploring the sidebar!
--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-// src/pages/index.tsx
-import React from "react";
-import AppLayout from "../layout/layout"; // <-- points to your layout.tsx
-import "../layout/layout.css";
-
-export default function Home() {
-  return <AppLayout />;
-}
